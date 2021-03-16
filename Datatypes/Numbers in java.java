e21a105c191225da3fcb0b8183e143a5226ea5b7
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:23:16 +0530

    Numbers in java

---
+++ Datatypes/Numbers in java.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Numbers in JAVA
+        System.out.println(21);
+        System.out.println(2 + 4);
+        System.out.println(6 * 9);
+        System.out.println(3 - 5);
+        System.out.println(10 / 2);
+    }
+}

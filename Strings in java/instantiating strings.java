c6b1329b3fc0374f55f939f47f741a0f2990ad1c
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:51:37 +0530

    instantiating strings

---
+++ Strings in java/instantiating strings.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String s1 = "Sunny Raj";
+        String s2 = "Sunny Raj";
+        String s3 = new String("Sunny Raj");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}

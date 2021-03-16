b708e33c9c0a411ed0cbd0c8a932bde15c15f681
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:32:02 +0530

    Create Newclass.java

---
+++ Packages , Classes and Methods in java/prog1/Newclass.java
@@ -0,0 +1,12 @@
+package com.SRaj;
+
+public class Newclass {
+    public static void main(String[] args) {
+        System.out.println("main method of Newclass");
+        sayHi();
+    }
+
+    public static void sayHi() {
+        System.out.println("sayHi method of Newclass");
+    }
+}

b996fb9ec12f76dd9159228ab7b3d3254bbfaddd
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:27:51 +0530

    String datatype

---
+++ Datatypes/String datatype.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String text = "String";
+        String newtext = text.toUpperCase();
+        System.out.println(text);
+        System.out.println(newtext);
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+    }
+}

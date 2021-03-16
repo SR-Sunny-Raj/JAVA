dcfb036c2fb4f49b4abfa0237b9570e19d3bdc2a
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:26:02 +0530

    char datatype

---
+++ Datatypes/char datatype.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        char c1 = 'A';
+        char c2 = 65;
+        char c3 = '\u0041';
+
+        int code1 = 'A';
+        int code2 = '\u0041';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(code1);
+        System.out.println(code2);
+    }
+}

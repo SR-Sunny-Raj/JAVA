a407238c7d4e94675fe07987f1f499002c11d3a1
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:46:32 +0530

    Create Sunny.java

---
+++ Packages , Classes and Methods in java/prog3/Sunny.java
@@ -0,0 +1,10 @@
+public class Sunny {
+    public static void main(String[] args) {
+        System.out.println("Before Hi");
+        sayHi();
+    }
+
+    public static void sayHi() {
+        System.out.println("Hi from Sunny");
+    }
+}

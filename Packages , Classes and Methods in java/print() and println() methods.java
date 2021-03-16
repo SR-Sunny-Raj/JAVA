fd2bf2543d1040bb2f734e8eaf810a70f5f7517a
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:48:12 +0530

    print() and println() methods

---
+++ Packages , Classes and Methods in java/print() and println() methods.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // println() method used to print on the screen and ln here moves to the next
+        // line
+        System.out.println("Sunny Raj");
+        System.out.println("24"); // this number is also a string
+        System.out.println(""); // blank space
+        System.out.println("Raj Sunny");
+        // print() method used to print everything in the same line
+        System.out.print("Sunny");
+        System.out.print(" ");
+        System.out.print("Raj");
+        System.out.print("123");
+    }
+}

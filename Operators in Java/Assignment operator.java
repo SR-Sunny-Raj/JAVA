8ffbaa7c39c4326e6d90b76328a5f6a63120899f
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:29:22 +0530

    Assignment operator

---
+++ Operators in Java/Assignment operator.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String myName = "Sunny Raj";
+        String myNameCopy = myName;
+        String myJob = "Student";
+        System.out.println(myNameCopy);
+        System.out.println(myJob);
+        myName = "Raj";
+        System.out.println(myName);
+    }
+}

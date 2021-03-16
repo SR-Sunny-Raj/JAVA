6d6e1e8cb4ff4f7de3ec1afec02d2ee9d7e6b09f
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:31:31 +0530

    Create main.java

---
+++ Packages , Classes and Methods in java/prog1/main.java
@@ -0,0 +1,9 @@
+package com.SRaj;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("main method of Main class");
+        Newclass.sayHi();
+    }
+}

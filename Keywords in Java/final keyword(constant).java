8a260cf1d5cb42886514a683c89311fe5c115d48
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:43:21 +0530

    final keyword(constant)

---
+++ Keywords in Java/final keyword(constant).java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        final String MY_NAME = "Sunny Raj";
+        // MY_NAME = "Newname"; //We cannot the the value of the constant
+        System.out.println(MY_NAME);
+    }
+}

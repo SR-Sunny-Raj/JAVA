697e4e8b0c6193c6aa60c5ebaeb83dbc8cf5ddf4
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-03-16 18:49:12 +0530

    boolean datatype

---
+++ Datatypes/boolean datatype.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = 2 < 3;
+        boolean b4 = 2 > 3;
+        boolean isAlive = true;
+        if (isAlive) {
+            System.out.println("Alive");
+        } else {
+            System.out.println("Not Alive");
+        }
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+    }
+}

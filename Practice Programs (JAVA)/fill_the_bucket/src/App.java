951eb7918f1d5674044fb238531a8790afdbe579
Sunny Raj <sraj1333.sr@gmail.com>
2022-03-16 11:46:50 +0530

    Practice Programs

--- Practice Programs (JAVA)/fill_the_bucket/src/App.java
+++ Practice Programs (JAVA)/fill_the_bucket/src/App.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+        while (T-- > 0) {
+            int K = input.nextInt(), X = input.nextInt();
+            System.out.println(K - X);
+        }
+        input.close();
     }
 }

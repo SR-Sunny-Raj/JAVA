7e868f8c76e963a49e1882b8767084ea1311a11c
Sunny Raj <sraj1333.sr@gmail.com>
2022-05-18 02:50:32 +0530

    Practice Programs

---
+++ Practice Programs (JAVA)/wip_mod1_3/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("The sum of " + args[0] + " and " + args[1] + " is "
+                + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
+    }
+}
